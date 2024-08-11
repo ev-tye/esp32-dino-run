5d4e044b3b1e685f1c4ed7759fbb2be525abfca1
Evan Tye <155539075+ev-tye@users.noreply.github.com>
2024-08-12 00:45:13 +0100

    Update esp32_dino_run.ino

--- esp32_dino_run.ino
+++ esp32_dino_run.ino
@@ -2,6 +2,8 @@
 // A port of the popular browser game to the ESP32 and a 16x2 LCD!
 // Author: Evan Tye - https://github.com/ev-tye
 
+// Copyright (c) Evan Tye 2024
+
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, 
 // including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished 
 // to do so, subject to the following conditions:
